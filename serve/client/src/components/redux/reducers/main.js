5a2b38d292492f64ada3571ae2673dfb6e36dfbf
Riya Jindal <122373500+riyaaaa123@users.noreply.github.com>
2023-05-14 21:01:16 +0530

    Delete serve/client directory

--- serve/client/src/components/redux/reducers/main.js
+++
@@ -1,7 +0,0 @@
-import { getProductsreducer } from "./Productsreducer";
-import { combineReducers } from "redux";
-const rootreducers = combineReducers({
-  getproductsdata: getProductsreducer
-});
-
-export default rootreducers;
